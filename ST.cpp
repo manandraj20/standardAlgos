1f44c4297d00cda9a4975d8154f30c8e44129a26
manandraj20 <manandraj20@iitk.ac.in>
2023-07-22 00:43:59 +0530

    codes added

--- ST.cpp
+++ ST.cpp
@@ -1,89 +1,48 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-class ST
+class SGTree
 {
-private:
-    vector<int> st, lazy;
-
+    vector<int> seg;
 public:
-    ST(int n)
-    {
-        st.resize(4 * n);
-        lazy.resize(4 * n);
-    }
-    void build(int ind, int low, int high, int arr[])
-    {
-        if (low == high)
-        {
-            st[ind] = arr[low];
-            return;
-        }
-        int mid = (low + high) >> 1;
-        build(2 * ind + 1, low, mid, arr);
-        build(2 * ind + 1, mid + 1, high, arr);
-        st[ind] = st[2 * ind + 1] + st[2 * ind + 2];
-    }
-    void updateLazy(int ind, int low, int high, int l, int r, int val){
-        // update the previous remaining updates and propogate downwards
-        if(lazy[ind]!=0){
-            st[ind] += (high - low + 1) * lazy[ind];
-            // propogate the lazy update downwards
-            // for the remaining nodes to get updated
-            if(low!=high){
-                lazy[2 * ind + 1]+= lazy[ind];
-                lazy[2 * ind + 2] += lazy[ind];
-            }
-            lazy[ind] = 0;
+	SGTree(int n) {
+		seg.resize(4 * n + 1);
+	}
 
-            // no overlap
-            //we dont do anything and return
-            // low high l r or l r low high
-            if(high < l or r< low){
-                return;
-            }
-            // complete overlap
-            // l low high r
-            // l low  high r
-            if(low>=l&&r>=high){
-                st[ind] += (high - low + 1) * val;
-                // if a non leaf node, it will have children
-                if(low!=high){
-                    lazy[2 * ind + 1] += val;
-                    lazy[2 * ind + 2] += val;
-                }
-                return;
-            }
-            // partial overlap
-            int mid = (low + high) >> 1;
-            updateLazy(2 * ind + 1, low, mid, l, r, val);
-            updateLazy(2 * ind + 2, mid + 1, high, l, r, val);
-            st[ind] = st[2 * ind + 1] + st[2 * ind + 2];
-        }
-    }
-    int queryLazy(int ind, int low, int high, int l, int r){
-        if(lazy[ind]!=0){
-            st[ind] += (high - low + 1) * lazy[ind];
-            // propogate the lazy update downwards
-            // for the remaining nodes to get updated
-            if(low!=high){
-                lazy[2 * ind + 1]+= lazy[ind];
-                lazy[2 * ind + 2] += lazy[ind];
-            }
-            lazy[ind] = 0;
-        }
-        // no overlap 
-        if(high < l or r< low){
-                return;
-            }
-            // complete overlap
-            // l low high r
-            if(low>=l&&r>=high){
-                return st[ind];
-            }
-            int mid = (low + high) >> 1;
-            int left = queryLazy(2 * ind + 1, low, mid, l, r);
-            int right = queryLazy(2 * ind + 2, mid + 1, high, l, r);
-            return left+right;
+	void build(int ind, int low, int high, int arr[]) {
+		if (low == high) {
+			seg[ind] = arr[low];
+			return;
+		}
 
-    }
+		int mid = (low + high) / 2;
+		build(2 * ind + 1, low, mid, arr);
+		build(2 * ind + 2, mid + 1, high, arr);
+		seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
+	}
+
+	int query(int ind, int low, int high, int l, int r) {
+		// no overlap
+		// l r low high or low high l r
+		if (r < low || high < l) return INT_MAX;
+
+		// complete overlap
+		// [l low high r]
+		if (low >= l && high <= r) return seg[ind];
+
+		int mid = (low + high) >> 1;
+		int left = query(2 * ind + 1, low, mid, l, r);
+		int right = query(2 * ind + 2, mid + 1, high, l, r);
+		return min(left, right);
+	}
+	void update(int ind, int low, int high, int i, int val) {
+		if (low == high) {
+			seg[ind] = val;
+			return;
+		}
+
+		int mid = (low + high) >> 1;
+		if (i <= mid) update(2 * ind + 1, low, mid, i, val);
+		else update(2 * ind + 2, mid + 1, high, i, val);
+		seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
+	}
 };
